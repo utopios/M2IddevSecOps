5a48802e6628538ae644e413ccc97e3ba34449fe
ihab abadi <utopios@imac-de-ihab.home>
2021-11-10 12:28:12 +0100

    suite terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,53 @@ provider "aws" {
     secret_key = var.AWS_SECRET_KEY
 }
 
-resource "aws_instance" "first_instance" {
+resource "aws_security_group" "instance_group" {
+    name = "test-group-terraform"
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+resource "aws_key_pair" "key_ssh" {
+    key_name = "test-terraform"
+    public_key = file("./ter.pub")
+}
+resource "aws_instance" "first_instance_3" {
     instance_type = "t2.micro"
     ami = "ami-0629230e074c580f2"
+    key_name = aws_key_pair.key_ssh.key_name
+    vpc_security_group_ids=[aws_security_group.instance_group.id]
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = file("./ter")
+        host = self.public_ip
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update"
+        ]
+    }
+provisioner "local-exec" {
+    command = "echo test > test.txt"
+}
+}
+
+
+# resource "aws_iam_user" "new_user" {
+#     count = length(var.AWS_USERS)
+#     name = var.AWS_USERS[count.index]
+# }
+
+output aws_ip_instance {
+  value       = aws_instance.first_instance_3.public_ip
+  
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -14,3 +14,7 @@ variable "AWS_REGION" {
   default     = "us-east-2"
   description = "aws region"
 }
+variable "AWS_USERS" {
+  type        = list
+  description = "list user to create"
+}
